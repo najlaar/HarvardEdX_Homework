d8a0164f25a981b6d4800b3fad7820271b1edc90
Najlaa Ramli <NAJLAAR@maxis.com.my>
2020-04-16 14:00:23 +0800

    copy UM exercise sample

---
+++ Code_0.R
@@ -0,0 +1,6 @@
+list_data=list(c("Red","Green","Black"),
+               list("Phyton","PHP","Java"))
+print("List:")
+print(list_data)
+print("Number of objects in the said list:")
+length(list_data)
